a96d855f1a8cd5703420eb90d2af2cbb605803d1
unknown <leon.pontes@laceti.cin.ufpe.br>
2021-08-10 00:10:37 -0300

    basic working template

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+const webpack = require("webpack");
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: ["webpack/hot/poll?100", "./src/index.ts"],
+  watch: true,
+  target: "node",
+  externals: [
+    nodeExternals({
+      whitelist: ["webpack/hot/poll?100"]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".tsx", ".ts" ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
+  }
+};
