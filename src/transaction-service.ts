aa22eee3ca1419bce4918cad39044e4df9389795
unknown <leon.pontes@laceti.cin.ufpe.br>
2021-08-12 01:14:36 -0300

    everything ok until goal #3.starting goal #4

--- src/transaction-service.ts
+++ src/transaction-service.ts
@@ -6,7 +6,8 @@ export const createTransaction = async (request:any): Promise<any> => {
 
   if (tran.product != "Fofa") {
     tran.feePerUse = 0;
-  }
+    console.log("Apenas o produto Pedra Fofa tem taxa por uso");
+  };
 
     tran.save((err:any, result:any) => {      
         if (err) {
@@ -20,7 +21,7 @@ export const createTransaction = async (request:any): Promise<any> => {
 
 export const getMargin = async (request:any): Promise<TotalTransaction> => {
   const clientData = request;
-  //
+  
   const filtered = await transactions.find({client : clientData.client , 
     transactionMonth: clientData.month, transactionYear:  clientData.year, product: clientData.product}).exec();
   
@@ -29,16 +30,23 @@ export const getMargin = async (request:any): Promise<TotalTransaction> => {
     return prev + ((cur.revenue*(1-(cur.taxes / 100))) - cur.productionCosts - cur.sellingCosts -
      cur.transportCosts + (cur.feePerUse*0.1*cur.revenue*(1-(cur.taxes / 100))));
   }, 0);
+
   const revenue = filtered.reduce(function(prev, cur){ return prev + cur.revenue}, 0);
+
   const prod = filtered.reduce(function(prev, cur){ return prev + cur.productionCosts}, 0);
+
   const sell = filtered.reduce(function(prev, cur){ return prev + cur.sellingCosts}, 0);
+
   const transp = filtered.reduce(function(prev, cur){ return prev + cur.transportCosts}, 0);
+
   const tax = filtered.reduce(function(prev, cur){ return prev + ((cur.taxes*cur.revenue)/100) +
      ((cur.taxes*cur.feePerUse*0.1*cur.revenue)/100)}, 0);
-  const useQuantRev = filtered.reduce(function(prev, cur){ return prev + cur.revenue}, 0);
 
-  let marginResult: TotalTransaction = {revenue: revenue, useQuantRev: useQuantRev, productionCosts: prod, sellingCosts: sell, 
-    transportCosts: transp, taxes: tax, margin: margin};
+  const useQuantRev = filtered.reduce(function(prev, cur){ return prev + 
+    cur.feePerUse*0.1*cur.revenue}, 0);
+
+  let marginResult: TotalTransaction = {revenue: revenue, useQuantRev: useQuantRev, 
+    productionCosts: prod, sellingCosts: sell, transportCosts: transp, taxes: tax, margin: margin};
 
   return marginResult;
 };
